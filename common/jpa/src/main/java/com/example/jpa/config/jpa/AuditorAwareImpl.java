4b9c364d5b920b40c2bc2752c5dfbc1fb322794e
well0924 <89343159+well0924@users.noreply.github.com>
2025-01-30 17:14:44 +0900

    feature : 디비 변경 및 audit 기능 추가
    - maria db -> mysql로 변경
    - jpa audit기능 처음에는 SYSTEM으로 등록 추후 api 사용시에는 회원 아이디로 생성자와 수정자가 들어감.

--- common/jpa/src/main/java/com/example/jpa/config/jpa/AuditorAwareImpl.java
+++ common/jpa/src/main/java/com/example/jpa/config/jpa/AuditorAwareImpl.java
@@ -1,5 +1,6 @@
 package com.example.jpa.config.jpa;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,16 +8,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
+@Slf4j
 public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
+        log.info("authentication::"+authentication.getName());
         if (authentication == null || !authentication.isAuthenticated()) {
             return Optional.of("SYSTEM");
         }
 
-        return Optional.of((String) authentication.getPrincipal());
+        return Optional.of(authentication.getName());
     }
 }
